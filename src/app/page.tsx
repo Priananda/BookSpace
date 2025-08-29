a29569c9bf4abf6c6306559618e85726371d3fd5
Your Name <you@example.com>
2025-08-29 18:34:28 +0800

    deploy V3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,8 @@
-'use client';
-
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import { Provider } from "react-redux";
-import { store } from './store';
-import { ThemeProvider } from "next-themes";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function HomePage() {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <Provider store={store}>
-              <Navbar />
-            <main className="p-4">{children}</main>
-             <Footer />
-          </Provider>
-        </ThemeProvider>
-      </body>
-    </html>
+    <main>
+      <h1>Welcome to BookSpace 🚀</h1>
+      <p>Silakan login untuk lanjut ke dashboard.</p>
+    </main>
   );
 }
